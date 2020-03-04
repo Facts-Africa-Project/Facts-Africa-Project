9895f8089dff7c15f554a7a54614beab6ec9a500
Mark Watitwa <markwatitwa98@gmail.com>
2020-03-04 09:18:52 +0300

    initial commit

---
+++ common/src/main/java/com/watitwa/common/CommonClass.java
@@ -0,0 +1,5 @@
+package com.watitwa.common;
+
+public class CommonClass {
+    public static String MESSAGE = "Hello from common class!!";
+}
---
+++ vendor/src/main/java/com/watitwa/factsafricavendor/MainActivity.java
@@ -0,0 +1,19 @@
+package com.watitwa.factsafricavendor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.watitwa.common.CommonClass;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView message = findViewById(R.id.message);
+        message.setText(CommonClass.MESSAGE + "to vendor activity");
+    }
+}
